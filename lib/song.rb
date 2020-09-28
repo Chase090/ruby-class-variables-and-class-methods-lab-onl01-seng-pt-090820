3c0405433fd181634bbb47c90b189a0cf6e1d388
Carl Michael Oyam <carloyam21@icloud.com>
2020-09-28 01:23:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,4 @@
 class Song 
   attr_accessor :name, :artist, :genre
- @@song_count = 0
- @@genre = []
-  
-  
-  def initialize
-    @@count += 1 
-  end
-  
-  def self.count
-    @@song_count
-  end
-  
-  
-  
   
 end
